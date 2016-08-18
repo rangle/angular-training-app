c1e37bd1015b36f240ffc6680b569514e7169d2f
Michael J. Bennett <michael.bennett@rangle.io>
2016-08-18 11:33:01 -0700

    (WIP) stubbed out form templates

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FORM_DIRECTIVES } from '@angular/forms';
 
 @Component({
+  directives: [ FORM_DIRECTIVES ],
   moduleId: module.id,
   selector: 'ata-editor',
   templateUrl: 'editor.component.html',
